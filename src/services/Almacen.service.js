21b1f8f391e6d6307a889967abf2f841fe5fa9fa
Cesar codigo <cmaytadatabaseg1@gmail.com>
2023-09-09 14:04:43 -0500

    front almacen listo

--- src/services/Almacen.service.js
+++ src/services/Almacen.service.js
@@ -22,21 +22,21 @@ class AlmacenService{
     }
 
     getOne(id){
-        return axios.get(API_URL+"/"+this.table_name+"/"+id)
+        return axios.get(API_URL+"/"+this.table_name+id+'/')
         .then(res=>{
             return res.data;
         })
     }
 
     updateOne(id,data){
-        return axios.put(API_URL+"/"+this.table_name+"/"+id,data)
+        return axios.put(API_URL+"/"+this.table_name+id+'/',data)
         .then(res=>{
             return res.data;
         })
     }
 
     deleteOne(id){
-        return axios.delete(API_URL+"/"+this.table_name+"/"+id)
+        return axios.delete(API_URL+"/"+this.table_name+id+'/')
         .then(res=>{
             return res.status
         })
